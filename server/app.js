245a0e8a666d3790ff49041d75924b1f9840ff54
ALI ZULFIQAR <alimalikali1928@gmail.com>
2025-04-01 03:35:07 +0500

    adding backend to vercel adding vercel.json file

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -16,9 +17,21 @@ const AppError = require('./utils/AppError');
 const formData = require('express-form-data');
 const multer = require('multer');
 
+
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // app init
 const app = express();
+
+
+
 // database connection
+connectToDB();
 
 // Security Middleware
 app.use(cors({ origin: process.env.ORIGIN, credentials: true, exposedHeaders: ['X-Total-Count'], methods: ['GET', 'POST', 'PATCH', 'DELETE'] }));
@@ -31,18 +44,17 @@ app.use(hpp()); // Prevent parameter pollution
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(multer({ limits: { fileSize: 10 * 1024 * 1024 } }).any()); // 10MB
 
 app.use(cookieParser());
 app.use(morgan('tiny'));
 
 // Rate limiting
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message: 'Too many requests from this IP, please try again in an hour!',
-// });
-// app.use('/api', limiter);
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 // Performance Middleware
 app.use(compression()); // Compress responses
@@ -57,3 +69,27 @@ app.use('*', (req, res, next) => {
 app.use(globalErrorHandler);
 
 module.exports = app;
+
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
+
+
+
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  app.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
+
